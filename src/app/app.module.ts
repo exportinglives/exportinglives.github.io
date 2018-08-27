dee4ce7d9b091aad279a9f8647131dd372fe05be
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-08-27 15:46:26 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CoverComponent } from './cover/cover.component';
+import { DocumentaryComponent } from './documentary/documentary.component';
+import { PetitionsComponent } from './petitions/petitions.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    CoverComponent,
+    DocumentaryComponent,
+    PetitionsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
