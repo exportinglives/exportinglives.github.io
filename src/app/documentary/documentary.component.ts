1854ed14e2b66243f9cd54a392d409f7f8799624
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-10-08 20:03:30 -0300

    Add small fix for language recognition

--- src/app/documentary/documentary.component.ts
+++ src/app/documentary/documentary.component.ts
@@ -10,7 +10,7 @@ export class DocumentaryComponent implements OnInit {
 
   ngOnInit() {
     const path = document.location.href.match(/\/([^\/]+)\/?$/)[1] || 'pt';
-    const closedCaptionsDefault = path !== 'pt' ? 1 : 0;
+    const closedCaptionsDefault = path === 'en' ? 1 : 0;
     // tslint:disable-next-line:max-line-length
     const newSrc = `https://www.youtube.com/embed/7ZXclVQC008?rel=0&amp;showinfo=0&amp;cc_lang_pref=${path}&amp;cc_load_policy=${closedCaptionsDefault}`;
     (<HTMLIFrameElement>document.getElementById('ytIframe')).src = newSrc;
