bc6c70358122de27b853f7e7c2843071d0208697
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-10-08 19:55:20 -0300

    Add default cc to youtube video based on the user language

--- src/app/documentary/documentary.component.ts
+++ src/app/documentary/documentary.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-documentary',
   templateUrl: './documentary.component.html',
-  styleUrls: ['./documentary.component.css']
+  styleUrls: ['./documentary.component.css'],
 })
 export class DocumentaryComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const path = document.location.href.match(/\/([^\/]+)\/?$/)[1] || 'pt';
+    const closedCaptionsDefault = path !== 'pt' ? 1 : 0;
+    // tslint:disable-next-line:max-line-length
+    const newSrc = `https://www.youtube.com/embed/7ZXclVQC008?rel=0&amp;showinfo=0&amp;cc_lang_pref=${path}&amp;cc_load_policy=${closedCaptionsDefault}`;
+    (<HTMLIFrameElement>document.getElementById('ytIframe')).src = newSrc;
   }
-
 }
